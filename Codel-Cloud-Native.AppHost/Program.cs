1d068a0037359ae2ba58781e07413d229f4eb36e
webreidi <webreidi@microsoft.com>
2024-08-20 14:50:25 -0700

    Adding mySQL connection

--- Codel-Cloud-Native.AppHost/Program.cs
+++ Codel-Cloud-Native.AppHost/Program.cs
@@ -1,12 +1,20 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.Codel_Cloud_Native_ApiService>("apiservice");
+var mysql = builder.AddMySql("mysql");
+var mysqldb = mysql.AddDatabase("mysqldb");
+
+var apiService = builder.AddProject<Projects.Codel_Cloud_Native_ApiService>("apiservice")
+                        .WithReference(mysqldb);
 
 builder.AddProject<Projects.Codel_Cloud_Native_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
+
+builder.AddMySqlDataSource("MySqConnection");
 
 builder.Build().Run();
