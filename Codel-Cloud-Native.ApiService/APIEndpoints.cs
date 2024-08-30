b467fb87604c7c9ac0fd9e3ccd9cf1696ac7579c
webreidi <webreidi@microsoft.com>
2024-08-30 12:30:49 -0700

    Database working, just need to get an indexoutofrange exception fixed.

---
+++ Codel-Cloud-Native.ApiService/APIEndpoints.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using System.Reflection.Metadata;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace Codele.ApiService;
+
+public static class ApiEndpoints
+{
+	
+	public static WebApplication WeatherForecastApi(this WebApplication app)
+	{
+		var summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+		
+		app.MapGet("/weatherforecast", () =>
+		{
+			var forecast = Enumerable.Range(1, 5).Select(index =>
+				new WeatherForecast
+				(
+					DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+					Random.Shared.Next(-20, 55),
+					summaries[Random.Shared.Next(summaries.Length)]
+				))
+				.ToArray();
+			return forecast;
+		});
+
+		return app;
+	}
+
+	public static WebApplication CodeleGameApi(this WebApplication app)
+	{
+
+		string[] words = [];
+
+		app.MapGet("/sample-data", async (SqlConnection db) =>
+		{
+			const string sql = """
+			            SELECT Id, Answer
+			            FROM Words
+			            """;
+			var Answers = await db.QueryAsync<Words>(sql);
+
+			var answer = Enumerable.Range(1, Answers.Count()).Select(index =>
+					new SampleData
+					(
+						words[Random.Shared.Next(words.Length)]
+					))
+					.ToArray();
+			return answer;
+		});
+
+		return app;
+	}
+}
+
+	record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+	{
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+	}
+
+	record SampleData(string answer);
+
+	public record Words(int Id, string Answer);
--- Codel-Cloud-Native.ApiService/Program.cs
+++ Codel-Cloud-Native.ApiService/Program.cs
@@ -1,99 +1,31 @@
-using System.Net.Sockets;
-using System.Reflection.Metadata;
-using Microsoft.Data.SqlClient;
-using Dapper;
+using Codele.ApiService;
 
-namespace Codele.ApiService;
+var builder = WebApplication.CreateBuilder(args);
 
-internal class Program
-{
-	private static void Main(string[] args)
-	{
-		var builder = WebApplication.CreateBuilder(args);
+// Add service defaults & Aspire components.
+builder.AddServiceDefaults();
 
-		builder.AddSqlServerClient("sqlserver");
+builder.AddSqlServerClient("codele");
 
-		// Add service defaults & Aspire components.
-		builder.AddServiceDefaults();
+// Add services to the container.
+builder.Services.AddProblemDetails();
+builder.Services.AddEndpointsApiExplorer();
 
-		// Add services to the container.
-		builder.Services.AddProblemDetails();
+var app = builder.Build();
 
-		builder.Services.AddProblemDetails();
-		builder.Services.AddEndpointsApiExplorer();
 
-		var app = builder.Build();
 
-		// Configure the HTTP request pipeline.
-		app.UseExceptionHandler();
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
 
-		var summaries = new[]
-		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
+app.WeatherForecastApi();
+app.CodeleGameApi();
 
-		string[] words = [];
+app.MapDefaultEndpoints();
 
-		app.MapGet("/sample-data", async(SqlConnection db) =>
-		{
-			const string sql = """
-			            SELECT Id, Answer
-			            FROM Words
-			            """;
-			var Answers = await db.QueryAsync<Words>(sql);
+app.Run();
 
-			var answer = Enumerable.Range(1, 14).Select(index =>
-					new SampleData
-					(
-						words[Random.Shared.Next(words.Length)]
-					))
-					.ToArray();
-			return answer;
 
-		});
 
-		//app.MapGet("/sample-data", async(MyDb1Context context) =>
-		//{
-		//	var answers = await context.Words.ToListAsync();
-		//	foreach (var word in answers)
-		//	{
-		//		words.Append(word.Answer);
-		//	}
-		//	var answer = Enumerable.Range(1, 14).Select(index =>
-		//			new SampleData
-		//			(
-		//				words[Random.Shared.Next(words.Length)]
-		//			))
-		//			.ToArray();
-		//	return answer;
-		//});
-
-		app.MapGet("/weatherforecast", () =>
-		{
-			var forecast = Enumerable.Range(1, 5).Select(index =>
-				new WeatherForecast
-				(
-					DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-					Random.Shared.Next(-20, 55),
-					summaries[Random.Shared.Next(summaries.Length)]
-				))
-				.ToArray();
-			return forecast;
-		});
-
-		app.MapDefaultEndpoints();
-
-		app.Run();
-	}
-}
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-record SampleData(string answer);
-
-public record Words(int Id, string Answer);
 
 
