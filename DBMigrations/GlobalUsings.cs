31c4255c7fe1c242b79fbb76d3479799d2a479f3
webreidi <webreidi@microsoft.com>
2024-09-06 09:32:30 -0700

    cleaning up old stuff

--- DBMigrations/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System.ComponentModel.DataAnnotations;
-global using Microsoft.EntityFrameworkCore;
-global using DBMigrations.Models;
-global using Codele.ApiService.Data;
-global using Pomelo.EntityFrameworkCore.MySql;
-global using System;
-global using Microsoft.Extensions.Configuration;
